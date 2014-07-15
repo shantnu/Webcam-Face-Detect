aa5d283bfea218ada7f184df655aefa4acfaf174
shantnu <a@a.com>
2014-07-15 21:06:13 +0100

    Minor improvemnts

--- webcam.py
+++ webcam.py
@@ -4,11 +4,11 @@ import sys
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-cap = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)
 
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, frame = video_capture.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
@@ -19,7 +19,7 @@ while(True):
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0) , 2)
 
         # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('Video',frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
