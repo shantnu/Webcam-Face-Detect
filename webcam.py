12155e803258ddef8e90135b5f26b8d8cf009536
fred <fredsan@localhost.localdomain>
2016-07-18 18:40:53 -0300

    modified

--- webcam.py
+++ webcam.py
@@ -1,10 +1,14 @@
 import cv2
 import sys
+import logging as log
+import datetime as dt
 
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
+log.basicConfig(filename='webcam.log',level=log.INFO)
 
 video_capture = cv2.VideoCapture(0)
+anterior = 0
 
 while True:
     # Capture frame-by-frame
@@ -16,16 +20,20 @@ while True:
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        minSize=(30, 30)
+        # flags=cv2.cv.CV_HAAR_SCALE_IMAGE
     )
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
+    if anterior != len(faces):
+        anterior = len(faces)
+        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+
     # Display the resulting frame
-    cv2.imshow('Video', frame)
+    # cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
